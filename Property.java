638512a330ccbc1b6f7c74d194b2c7c60e7b890d
chullenger0825 <52304073+chullenger0825@users.noreply.github.com>
2019-07-01 23:44:46 +0800

    Update Property.java

--- Property.java
+++ Property.java
@@ -48,6 +48,14 @@ public class Property extends Buyable{
   public void develop(){
     nHouses += 1;
   }
+  
+  /**
+    adds 1 to nHouses when player decides to develop on int
+    nhouse = 1, player chose to develop, nhouse = 2
+    nhouse = 0, player chose to develop, nhouse = 1
+    nhouse = 2, player chose to develop, nhouse = 3
+    No params needed (will mostly use constructors in subclasses)
+  */
 
   public double getRent(){
     return rentTiers[nHouses];
@@ -58,7 +66,14 @@ public class Property extends Buyable{
       rentTiers[i] += 10;
     }
   }
-
+/**
+    add the rent when property owns have same color with other property owned. 
+      Almond Drive = 2 when kasoy Street is bought Almond Drive = 12
+      Solar Street = 22 when Galaxy Street is bought Solar Street = 22
+      Juan Luna = 10 when Ylaya is bought Juan Luna = 20
+      
+    No params needed (will mostly use constructors in subclasses)
+  */
   public int getFootLimit(){
     return nFootTrafficLimit;
   }
@@ -66,19 +81,49 @@ public class Property extends Buyable{
   public void playerLanded(){
     nFootTraffic += 1;
   }
+  /**
+    tracks the number of player landed on a specific area 
+      when a player is on Juan Luna nFootTraffic = 1 
+      when another player lands nFootTraffic = 2
+      when another player lands nFootTraffic = 3
+    No params needed (will mostly use constructors in subclasses)
+  */
 
   public double buyProperty(Player buyer){
     this.owner = buyer;
     return dBuyPrice;
   }
+   /**
+    tracks the footTraffic 
+      when a player is on Juan Luna nFootTraffic = 1 
+      when another player lands nFootTraffic = 2
+      when another player lands nFootTraffic = 3
+    No params needed (will mostly use constructors in subclasses)
+  */
 
   public double collectRent(){
     dCollected = rentTiers[nHouses];
     return rentTiers[nHouses];
   }
+   /**
+    Collects rent base from the property and the level
+      player1 steps on Madison unimporved owned by player 2 pays 14
+      player1 steps on 9th Street unimporved owned by player 2 pays 26
+      player1 steps on 5th avenue unimporved owned by player 2 pays 28
+      
+    No params needed (will mostly use constructors in subclasses)
+  */
 
   public void debugPrint(){ //FOR DEBUGGING PURPOSES ONLY
     System.out.println("" + this.strPropertyName + " " + this.color + " " + this.dBuyPrice + " " + this.dDevelopCost  + " " + this.rentTiers[nHouses] + " " +  this.dFootMultiplier);
   }
+  /**
+    debugging purposes
+     Almond Drive grey 60 50 1 2.5
+     Rodeo Drive Purple 100 50 2 3.5
+     Juan Luna Pink 140 100 2 4.5
+    No params needed (will mostly use constructors in subclasses)
+  */
 
 }
+
